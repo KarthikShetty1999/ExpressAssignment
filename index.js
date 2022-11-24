43253259c8fc9b1c20c51c62f2d09c1faf0cbe5e
Karthik170 <karthikkumar.shetty@niveussolution.com>
2022-11-24 12:56:09 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParse = require('body-parser');
+const port = 2000;
+app.use(bodyParse.json())
+
+//Creating a endpoint of get
+app.get('/', (req,res)=>{
+    let query = req.query
+    let path = req.path
+    console.log(query)
+    console.log(path)
+    res.json({"info":{query,path}})
+})
+
+//Creating a endpoint of post
+app.post('/postData',(req,res)=>{
+    let data = [];
+    data.push(req.body.id)
+    data.push(req.body.name)
+    res.send(data)
+})
+
+app.listen(port,()=>{
+    console.log('listening to server http://localhost:',port);
+})
